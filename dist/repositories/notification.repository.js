03a09ef2fdc01eb1d96dbc05c9a14ae63c309404
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-05-30 17:46:26 +0300

    error for run build for server

--- dist/repositories/notification.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NotificationRepository = void 0;
-const data_source_1 = require("../dbConfig/data-source");
-const Notification_1 = require("../entities/Notification");
-exports.NotificationRepository = data_source_1.AppDataSource.getRepository(Notification_1.Notification);
