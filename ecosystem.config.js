c39eef0286f67e0b211b8ea1a4d2aa0afd3b414b
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-06-02 09:25:15 +0300

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
-module.exports = {
+export default {
   apps: [{
     name: "growup-api",
-    script: "dist/index.js",  // Your compiled JS entry point
-    instances: "max",         // Use all CPU cores
-    autorestart: true,       // Auto-restart if crashes
-    watch: false,            // Disable watching files
-    max_memory_restart: "1G",// Restart if memory exceeds 1GB
+    script: "dist/index.js",
+    instances: "max",
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    interpreter_args: "--experimental-specifier-resolution=node", // هذا السطر المهم
     env: {
       NODE_ENV: "production",
-      DB_HOST: "your_db_host",
-      // Add other environment variables
+      DB_HOST: "your_db_host"
     }
   }]
-};
+}
