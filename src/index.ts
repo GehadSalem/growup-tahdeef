65d685c4e1a2e19c28efeec0e34f263d924fa660
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2025-06-02 11:11:16 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,11 @@ dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
-
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
+allowedOrigins.push('http://31.97.55.57');
 // CORS configuration
 const corsOptions = {
-  origin: true,
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
